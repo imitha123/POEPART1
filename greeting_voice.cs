96e0758b95e1c3297d55bbb1604ea881233d5db4
Imitha Maneli <ST10483606@rcconnect.edu.za>
2026-05-28 16:37:23 +0200

    Add the voice greeting

--- greeting_voice.cs
+++ greeting_voice.cs
@@ -1,6 +1,42 @@
-﻿namespace POEPART1
+﻿using System;
+using System.IO;
+using System.Media;
+
+namespace POEPART1
 {
     internal class greeting_voice
     {
+        //my constructor
+        public greeting_voice()
+        {
+            // full path of the recording
+            string path_directory = AppDomain.CurrentDomain.BaseDirectory;
+            //back track to get the exact location of the record
+            string recordPath = path_directory.Replace("\\bin\\Debug", "");
+            // combine the location and the recording it's to play it
+            string record = Path.Combine(recordPath, "Recording.wav");
+            play_voice(record);
+        }
+        // method to play the voice recording
+        public void play_voice(string voice)
+        {
+            // error handling
+            try
+            {
+                // use the sound player class to play the recording
+                using (SoundPlayer speechObj = new SoundPlayer(voice))
+                {
+                    speechObj.PlaySync();
+                }
+
+            }
+            // if the file is not found or any other error occurs, it will be caught here
+            catch (Exception e)
+            {
+                // error message will be displayed in the console
+                Console.WriteLine($"{e.Message}");
+
+            }
+        }
     }
 }
